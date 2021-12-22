c59c5e3fb89ad3958f26913ec2605374bbe7debb
Enzo Gastón Milano <gastonmilano10@gmail.com>
2021-12-22 01:33:32 -0300

    Nueva version login
    Se desarrollo una nueva version de login, orientada a dispositivos moviles

--- Client/projectoneclient/src/components/Login.vue
+++ Client/projectoneclient/src/components/Login.vue
@@ -1,64 +1,105 @@
 <template>
   <v-app id="inspire">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md6>
-            <v-card class="elevation-12">
-              <v-row>
-                <v-col md="6">
-                  <v-card-text class="mt-6 pb-0">
-                    <v-form>
-                      <v-text-field
-                        width="150"
-                        name="user"
-                        label="Usuario"
-                        type="text"
-                        placehoder="Usuario"
-                        prepend-icon="mdi-account"
-                        solo
-                      ></v-text-field>
-                      <v-text-field
-                        name="password"
-                        label="Password"
-                        type="password"
-                        placehoder="Contraseña"
-                        prepend-icon="mdi-lock"
-                        append-icon="mdi-eye"
-                        solo
-                      ></v-text-field>
-                    </v-form>
-                  </v-card-text>
-                  <v-card-actions class="pt-0">
-                    <v-col class="text-right">
-                      <v-btn color="primary">Ingresar</v-btn>
-                    </v-col>
-                  </v-card-actions>
-                </v-col>
+    <div class="app-container">
+      <v-card elevation="5" class="login-container pa-3">
+        <v-container fluid class="text-center">
+          <h1>Ingresar</h1>
 
-                <v-col class="cardPanel">
-                  <h1 class="white--text">Ingresar</h1>
-                </v-col>
-              </v-row>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+          <v-form v-on:submit.prevent="login">
+            <v-text-field
+              class="mt-5"
+              append-icon="mdi-account"
+              label="Usuario / correo"
+              solo
+              v-model="user"
+            ></v-text-field>
+
+            <v-text-field
+              class="mt-n5"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show1 ? 'text' : 'password'"
+              label="Contraseña"
+              @click:append="show1 = !show1"
+              solo
+              v-model="password"
+            ></v-text-field>
+
+            <v-btn block color="primary" type="submit"> Ingresar </v-btn>
+          </v-form>
+
+          <h5 class="mt-5 mb-2">O ingresar con:</h5>
+
+          <v-btn class="mx-2" fab dark small color="primary">
+            <v-icon dark> mdi-facebook </v-icon>
+          </v-btn>
+
+          <v-btn fab dark small color="error">
+            <v-icon dark> mdi-google </v-icon>
+          </v-btn>
+
+          <h5 class="mt-5 mb-2">
+            Si no tienes usuario, <a>REGISTRATE AQUI</a>
+          </h5>
+          <a>CONTINUAR SIN USUARIO</a>
+        </v-container>
+      </v-card>
+    </div>
   </v-app>
 </template>
 
 <script>
+import Axios from "axios";
+
 export default {
   name: "Login",
   props: {
     source: String,
   },
+
+  data() {
+    return {
+      show1: false,
+      user: "",
+      password: "",
+      error: false,
+      error_msj: false,
+    };
+  },
+
+  methods: {
+    login() {
+      let jsonLogin = {
+        user: this.user,
+        password: this.password,
+      };
+
+      Axios.post('https://localhost:5001/api/Users/Login', jsonLogin)
+      .then(response => console.log(response))
+      console.log(jsonLogin);
+    },
+  },
 };
 </script>
 
 <style scoped>
-.cardPanel {
-  background-color: #539e9e;
+.app-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgb(238, 174, 202);
+  background: radial-gradient(
+    circle,
+    rgba(238, 174, 202, 1) 0%,
+    rgba(148, 187, 233, 1) 100%
+  );
+}
+
+.login-container {
+  width: 60%;
+  max-width: 350px;
+  display: flex;
+  justify-content: center;
+  border-radius: 5px;
 }
 </style>
--- Client/projectoneclient/src/main.js
+++ Client/projectoneclient/src/main.js
@@ -3,8 +3,13 @@ import Login from './components/Login.vue'
 import vuetify from './plugins/vuetify'
 import store from './store'
 import VueRouter from 'vue-router'
+import VueAxios from 'vue-axios'
+import Axios from 'axios'
+import "../src/main.scss"
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, Axios);
+Vue.use(VueRouter);
 
 new Vue({
   vuetify,
@@ -12,5 +17,3 @@ new Vue({
   render: h => h(Login)
 }).$mount('#app')
 
-
-Vue.use(VueRouter)
